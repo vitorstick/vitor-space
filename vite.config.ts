e1ca5396ff6bd4d7c9763124a10b7ba847dce88c
Vitor Ferreira <vitorstick@gmail.com>
2026-08-16 18:46:09 +0200

    feat: initialize React root with BrowserRouter and reset base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vitor-space/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
